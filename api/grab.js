916850e807ba18d516ef9cdf73f07c8e5de0f973
xercessss <xercess@hotmail.fr>
2024-12-09 16:31:24 +0000

    Update grab.js

--- api/grab.js
+++ api/grab.js
@@ -3,10 +3,10 @@ export default async function handler(req, res) {
   const { exec } = require("child_process");
 
   // Replace with your actual npm command
-  exec("npm run grab -- --site=bein.com --lang=en", (error, stdout, stderr) => {
-    if (error) {
-      return res.status(500).json({ error: stderr });
-    }
-    res.status(200).json({ output: stdout });
-  });
-}
+
+exec("npm config set cache /tmp/.npm && npm run grab -- --site=bein.com --lang=en", (error, stdout, stderr) => {
+  if (error) {
+    return res.status(500).json({ error: stderr });
+  }
+  res.status(200).json({ output: stdout });
+});
